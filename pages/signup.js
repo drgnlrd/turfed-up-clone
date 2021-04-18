2f107c30233f02f6ab3b3699046b04f799a89030
iamgadds <husain.ghadiyali07@gmail.com>
2021-04-18 14:29:19 +0530

    removed additional files

--- pages/signup.js
+++ pages/signup.js
@@ -4,15 +4,30 @@ import firebaseClient from '../firebaseClient';
 import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
-import shortid from 'shortid'
 import emailjs from 'emailjs-com';
 import otpGenerator from 'otp-generator';
 
-import { Box, Flex, Button, Input, FormControl, FormLabel, FormHelperText, Stack, Heading, useToast } from "@chakra-ui/react";
+import {
+    Box,
+    Flex,
+    Stack,
+    Heading,
+    Text,
+    Container,
+    Input,
+    Button,
+    SimpleGrid,
+    Avatar,
+    AvatarGroup,
+    useBreakpointValue,
+    IconProps,
+    Icon,
+    FormControl, FormLabel, FormHelperText, useToast
+  } from '@chakra-ui/react';
 
-export default function signup() {
-    firebaseClient();
-    const toast = useToast();
+  
+  export default function signup() {
+
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
     const [name, setName] = useState("");
@@ -21,6 +36,8 @@ export default function signup() {
     const [verifyId, setVerifyId] = useState("");
 
     var db = firebase.firestore();
+    firebaseClient();
+    const toast = useToast();
 
     useEffect(() => {
         setVerifyId(otpGenerator.generate(6, { upperCase: false, specialChars: false, alphabets: false }));
@@ -33,95 +50,265 @@ export default function signup() {
         message: verifyId,
    }
 
-   const handleSubmit = async() => {
-    console.log(verifyId);
-    if(otp == verifyId) {
-        await firebase.auth().createUserWithEmailAndPassword(email,pass).then(() => {
-            db.collection('userData').add({
-                name: name,
-                email: email,
-                mobile: mobile,
-                bookings:"",
-            })
-        })
-        .then(function() {
-            window.location.href = '/'
-        }).catch(function (error) {
-            const message = error.message;
-            toast({
-                title: 'An error occured',
-                descripion: message,
-                status: 'error',
-                duration: 9000,
-                isClosed: true
-            });
-        });
-        console.log('otp verified');
-    } 
-    else {
-        alert('wrong otp!!');
-    }    
-}
+    const handleSubmit = async() => {
+      console.log(verifyId);
+      if(otp == verifyId) {
+          await firebase.auth().createUserWithEmailAndPassword(email,pass).then(() => {
+              db.collection('userData').add({
+                  name: name,
+                  email: email,
+                  mobile: mobile,
+                  bookings: [],
+              })
+          })
+          .then(function() {
+              window.location.href = '/'
+          }).catch(function (error) {
+              const message = error.message;
+              toast({
+                  title: 'An error occured',
+                  descripion: message,
+                  status: 'error',
+                  duration: 9000,
+                  isClosed: true
+              });
+          });
+          console.log('otp verified');
+      } 
+      else {
+          alert('wrong otp!!');
+      }    
+  }
 
-    return(
-        <Flex>
-            <Box w={500} p={4} my={12} mx='auto'>
-                <Heading as='h2' textAlign='center'>
-                    SignUp
-                </Heading>
-                <FormControl isRequired>
-                    <FormLabel htmlFor='email'> Email Address</FormLabel>
-                    <Input onChange={(e) => setEmail(e.target.value)}
-                    type='email' id='emailAddress' value={email}
-                    aria-describedby='email-helper-text' />
+    return (
+      <Box minH={'100vh'} position={'relative'} bg={['url("/signbg.jpg")','none']}>
+        <Container
+          as={SimpleGrid}
+          maxW={'7xl'}
+          columns={{ base: 1, md: 2 }}
+          spacing={{ base: 10, lg: 32 }}
+          py={{ base: 10, sm: 20, lg: 32 }}>
+          <Stack spacing={{ base: 10, md: 20 }}>
+            <Heading
+              lineHeight={1.1}
+              fontSize={{ base: '5xl', sm: '4xl', md: '5xl', lg: '6xl' }}
+              zIndex={'10'}>
+              Lets Get{' '}
+              <Text
+                as={'span'}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                bgClip="text">
+                Turfed Up!
+              </Text>
+            </Heading>
+            <Stack direction={'row'} spacing={4} align={'center'}>
+              
+              {/* <Text fontFamily={'heading'} fontSize={{ base: '4xl', md: '6xl' }}>
+                +
+              </Text>
+              <Flex
+                align={'center'}
+                justify={'center'}
+                fontFamily={'heading'}
+                fontSize={{ base: 'sm', md: 'lg' }}
+                bg={'gray.800'}
+                color={'white'}
+                rounded={'full'}
+                width={useBreakpointValue({ base: '44px', md: '60px' })}
+                height={useBreakpointValue({ base: '44px', md: '60px' })}
+                position={'relative'}
+                _before={{
+                  content: '""',
+                  width: 'full',
+                  height: 'full',
+                  rounded: 'full',
+                  transform: 'scale(1.125)',
+                  bgGradient: 'linear(to-bl, orange.400,yellow.400)',
+                  position: 'absolute',
+                  zIndex: -1,
+                  top: 0,
+                  left: 0,
+                }}>
+                YOU
+              </Flex> */}
+            </Stack>
+          </Stack>
+          <Stack
+            bg={'gray.50'}
+            rounded={'xl'}
+            p={{ base: 4, sm: 6, md: 8 }}
+            spacing={{ base: 8 }}
+            maxW={{ lg: 'lg' }}
+            boxShadow="lg">
+            <Stack spacing={4}>
+              <Heading
+                color={'gray.800'}
+                lineHeight={1.1}
+                fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+                Sign Up
+                <Text
+                  as={'span'}
+                  bgGradient="linear(to-r, red.400,pink.400)"
+                  bgClip="text">
+                  !
+                </Text>
+              </Heading>
+              
+            </Stack>
+            <Box as={'form'} mt={10} >
+              <Stack spacing={4}>
+              <FormControl isRequired>
+                <Input
+                  placeholder="Name"
+                  bg={'gray.100'}
+                  border={0}
+                  color={'gray.500'}
+                  _placeholder={{
+                    color: 'gray.500',
+                  }}
+                  onChange={(e) => setName(e.target.value)}
+                    type='input' 
+                    id='name' 
+                    value={name}
+                    aria-describedby='name-helper-text'
+                />
                 </FormControl>
                 <FormControl isRequired>
-                    <FormLabel htmlFor='password'> Password</FormLabel>
-                    <Input onChange={(e) => setPass(e.target.value)}
-                    type='password' id='pass' value={pass}
-                    aria-describedby='password-helper-text' />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel htmlFor='name'> Name</FormLabel>
-                    <Input onChange={(e) => setName(e.target.value)}
-                    type='input' id='name' value={name}
-                    aria-describedby='name-helper-text' />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel htmlFor='mobile'> Mobile Number</FormLabel>
-                    <Input onChange={(e) => setMobile(e.target.value)}
+                <Input
+                  placeholder="Mobile Number"
+                  bg={'gray.100'}
+                  border={0}
+                  color={'gray.500'}
+                  _placeholder={{
+                    color: 'gray.500',
+                  }}
+                  onChange={(e) => setMobile(e.target.value)}
                     type='text' id='pass' value={mobile}
-                    aria-describedby='mobile-helper-text' />
+                    aria-describedby='mobile-helper-text'
+                />
                 </FormControl>
-
                 <FormControl isRequired>
-                    <FormLabel htmlFor='otp'> Enter otp</FormLabel>
-                    <Input onChange={(e) => setOtp(e.target.value)}
-                    type='text' id='otp' value={otp}
-                    aria-describedby='otp-helper-text' />
+                <Input
+                  placeholder="Password"
+                  bg={'gray.100'}
+                  border={0}
+                  color={'gray.500'}
+                  _placeholder={{
+                    color: 'gray.500',
+                  }}
+                  onChange={(e) => setPass(e.target.value)}
+                    type='password' id='pass' value={pass}
+                    aria-describedby='password-helper-text'
+                />
                 </FormControl>
-
-                <Button isDisabled={email === '' || pass === '' || name === '' || mobile === ''} 
+                <FormControl isRequired>
+                <Input
+                  placeholder="Email"
+                  bg={'gray.100'}
+                  border={0}
+                  color={'gray.500'}
+                  _placeholder={{
+                    color: 'gray.500',
+                  }}
+                  onChange={(e) => setEmail(e.target.value)}
+                    type='email' 
+                    id='emailAddress' 
+                    value={email}
+                    aria-describedby='email-helper-text'
+                />
+                </FormControl>
+                <FormControl isRequired>
+                <Input
+                  placeholder="Generate OTP"
+                  bg={'gray.100'}
+                  border={0}
+                  color={'gray.500'}
+                  _placeholder={{
+                    color: 'gray.500',
+                  }}
+                  onChange={(e) => setOtp(e.target.value)}
+                    type='text' 
+                    id='otp' 
+                    value={otp}
+                    aria-describedby='otp-helper-text'
+                />
+                </FormControl>
+              </Stack>
+              <Button
+                fontFamily={'heading'}
+                mt={8}
+                w={'full'}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                color={'white'}
+                _hover={{
+                  bgGradient: 'linear(to-r, red.400,pink.400)',
+                  boxShadow: 'xl',
+                }}
+                isDisabled={email === '' || pass === '' || name === '' || mobile === ''} 
                 onClick = {() => {
                     emailjs.send('service_z6cltgd', 'template_jtkla8g', emailToUser, 'user_2aLKtYP7rnIjbt16dV8lj')
-                }} >
-                    Generate Otp
+                }}>
+                Generate OTP
+              </Button>
+              <Button
+              fontFamily={'heading'}
+                mt={8}
+                w={'full'}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                color={'white'}
+                _hover={{
+                  bgGradient: 'linear(to-r, red.400,pink.400)',
+                  boxShadow: 'xl',
+                }}
+                isDisabled={email === '' || pass === '' || name === '' || mobile === '' || otp === '' }
+                onClick={handleSubmit}>
+                  Sign In
+              </Button>
+              <Button
+              fontFamily={'heading'}
+                mt={8}
+                w={'full'}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                color={'white'}
+                _hover={{
+                  bgGradient: 'linear(to-r, red.400,pink.400)',
+                  boxShadow: 'xl',
+                }}>
+                  <Link href='./login'>
+                    <a>Login</a>
+                  </Link>
                 </Button>
-                
-                <Stack justify='center' mt={6} isinLine spacing={10}>
-                    <Button minWidth='40%' variant='solid' variantcolor='blue'
-                    isDisabled={email === '' || pass === '' || name === '' || mobile === '' || otp === '' }
-                        onClick={handleSubmit}
-                        >
-                            Create Account
-                        </Button>
-                        <Button minWidth='40%' variant='solid' variantColor='blue'>
-                            <Link href='./login'>
-                                <a>Login</a>
-                            </Link>
-                        </Button>
-                </Stack>
             </Box>
-        </Flex>
-    )
-}
+            form
+          </Stack>
+        </Container>
+        <Blur
+          position={'absolute'}
+          top={-10}
+          left={-10}
+          style={{ filter: 'blur(70px)' }}
+        />
+      </Box>
+    );
+  }
+  
+  export const Blur = (props) => {
+    return (
+      <Icon
+        width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+        zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
+        height="560px"
+        viewBox="0 0 528 560"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}>
+        <circle cx="71" cy="61" r="111" fill="#F56565" />
+        <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+        <circle cy="291" r="139" fill="#ED64A6" />
+        <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+        <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+        <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+        <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+      </Icon>
+    );
+  };
