de79bb97078d0216920ac8e925f99ba32b87a2d1
drgnlrd <burhanhp5158@gmail.com>
2021-04-19 13:15:14 +0530

    done for sure

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,27 +3,5 @@ const stripe = require('stripe')('sk_test_51Ifj2ESIx03WM52JVP64h9S6mjSJlSGIp2V9S
 
 
 export default (req, res) => {
-  if(req.method == POST){
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000,
-          },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: 'localhost:3000/success',
-      cancel_url: 'localhost:3000/cancel',
-    });
-  
-    res.json({ id: session.id });
-  
-  }
+  console.log('res');
 }
