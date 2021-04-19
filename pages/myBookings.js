470992a41b0ae4a378e5968f4d47cac89c2f7e05
iamgadds <husain.ghadiyali07@gmail.com>
2021-04-19 05:43:27 +0530

    myBookings done

--- pages/myBookings.js
+++ pages/myBookings.js
@@ -1,13 +1,28 @@
-import React,{ useState, useEffect} from 'react';
+
+import {
+    Flex,
+    Stack,
+    Heading,
+    Text,
+    Input,
+    Button,
+    Icon,
+    Box,
+    useColorModeValue,
+    createIcon,
+    Container,
+    Divider,
+    useToast,
+    Link
+  } from '@chakra-ui/react';
+  import React,{ useState, useEffect} from 'react';
 import nookies from "nookies";
 import {verifyIdToken} from '../firebaseAdmin';
 import firebaseClient from '../firebaseClient';
 import 'firebase/auth';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import {Box, Flex, Text, Heading, Button, Link} from '@chakra-ui/react';
 import Layout from '../components/Container';
-
 
 function renderTurf(email){
     const [Link, setLink] = useState([]);
@@ -28,93 +43,77 @@ function renderTurf(email){
     return Link
 }
 
-const myBookings = ({email}) => {
-    
-    const turf = renderTurf(email);
-    turf.map(res => {
-        console.log(res.bookings);
-        res.bookings.map(booking => {
-            console.log(booking);
-        })
-    })
-    
-    console.log(turf);
+const temp = ({email}) => {
 
+    const turf = renderTurf(email);
     return (
         <Layout>
-        <Flex>
-            <Box w={600} p={4} my={12} mx='auto'>
-                <Text>Your bookings:</Text>
-                {/* {
-                    link.map((booking, id) => (
-                        <Box key={id}>
-                            <Text>{booking.date}</Text>
-                            <Text>{booking.turfId}</Text>
-                        </Box>
-                    ))
-                } */}
-
-                {
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        py={12}
+        >
+        <Container maxW={'container.sm'}>
+        <Stack
+          boxShadow={'2xl'}
+          bg={useColorModeValue('rgba(255,255,255,0.1)', 'rgba(0,0,0,0.6)')}
+          rounded={'xl'}
+          p={10}
+          spacing={8}
+          align={'center'}>
+          <Stack align={'center'} spacing={2}>
+            <Heading
+              textTransform={'uppercase'}
+              fontSize={'3xl'}
+              color={useColorModeValue('gray.800', 'gray.200')}>
+             Your Bookings:
+            </Heading>
+          </Stack>
+          <Stack spacing={4} direction={{ base: 'column', md: 'column' }} w={'full'}>
+            
+          {
                     turf.map((item, id) => (
-                        <Box key={id}>
+                        <Box key={id}
+                        >
                             {
                                 item.bookings.map((res,id) => (
-                                    <Box key={id}>
+                                  <>
+                                    <Box key={id}
+                                    bg={useColorModeValue('rgba(255,255,255,0)', 'rgba(0,0,0,0.1)')} boxShadow={'2xl'} rounded={'xl'}
+                                    p={10}
+                                    spacing={8}
+                                    >
                                     <Text>{res.date}</Text>
-                                    <Text> {res.turfName} </Text>
-                                    <Text> {res.timings} </Text>
+                                    <Text ><Text fontWeight={'700'} as={'span'}>Turf Name:</Text>&nbsp; {res.turfName}  </Text>
+                                    <Text ><Text fontWeight={'700'} as={'span'}>Timings</Text>&nbsp; {res.timings}  </Text>
                                     </Box>
+                                    <Divider />
+                                    </>
                                 ))
                             }
                         </Box>
                     )) 
                 }
 
-            </Box>
-        </Flex>
-        </Layout>
-    )
-
+          </Stack>
+        </Stack>
+        </Container>
+      </Flex>
+      </Layout>
+    );
 }
 
 export async function getServerSideProps(context){
-    // try {
         
         const cookies = nookies.get(context);
         const token = await verifyIdToken(cookies.token);
         const {email} = token;
-        // const link = {}
-        // const db = firebase.firestore();
-
-        // db.collection('userData').where('email','==',email).onSnapshot((snapshot) => {
-        //     const newUsers = snapshot.docs.map((doc) => ({
-        //         id:doc.id,
-        //     }))
-        //     link['url'] = newUsers;
-        // })
-
-        // db.collection('userData').doc(link.url).get().then((snapshot) => {
-        //     link['users'] = snapshot.data();
-        // })
-        // if(link.users == undefined) {
-        //     return { props: {
-        //         link: null
-        //     }
-        //     }
-        // }
-        // else{
         return {
             props: {
                 email: email,
             }
         };
-    // }
-    // } 
-    // catch (err){
-    //     // context.res.writeHead(302, { location: '/login' });
-    //     context.res.end();
-    //     return { props : [] };
-    // }
 }
 
-export default myBookings;
+export default temp;
